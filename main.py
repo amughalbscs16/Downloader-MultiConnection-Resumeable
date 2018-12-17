a5023cbc9b94eba5c0a2b95fc51762441149af02
amughalbscs16 <amughal.bscs16seecs@seecs.edu.pk>
2018-12-17 17:06:00 +0500

    Parsing Command Line Arguments and basic cleaning of data done

--- main.py
+++ main.py
@@ -1,80 +1,102 @@
 import sys
 from socket import *
-import time
+import time;
+filesize=0;
 def makeTcpIpSocket():
     return socket(AF_INET,SOCK_STREAM)
 
-def makeUdpIpSocket():
-    return socket(AF_INET, SOCK_DGRAM)
-
 def makeTcpConnect(socket,ip,port):
     socket.connect((ip,int(port)));
-    
-def downloadsingletcp(socket1,down_file_dir,location_to_save):
-    return;
 
 def makeTcpRequest(socket,down_file_dir,host):
-    socket.send(bytes("GET "+down_file_dir+" HTTP/1.0\r\nHost: "+host+"\r\n\r\n",'utf-8'))
+    socket.send(bytes("GET "+down_file_dir+" HTTP/1.0\r\nHost: "+host+"\r\nRange: bytes=10-\r\n\r\n",'utf-8'))
 
-def makeUdpRequest(my_ip,socket,down_file_dir,host):
-    socket.bind(("",19995))
-    
 def downloadTcpFile(socket,down_file):
     count = 0;
     headers = "";
     with open(down_file, 'wb') as f:
-    
-                while True:
-                    data = ""
-                    resp = socket.recv(1024)
+        resp = socket.recv(1024)
+        print(resp)
+        data = ""
+        headers = resp.decode("ASCII").split("\r\n\r\n")[0];
+        filesize=int(headers.split("\r\n")[6].split(":")[1])
+        print(filesize)
+        return 0;
+    """    while True:
                     if resp.decode("ASCII") == "":
                         break
                         f.close();
                     if count == 0:
                         count+=1
-                        headers = resp.decode("ASCII").split("\r\n\r\n")[0];
+                        
                         data = resp.decode("ASCII").split("\r\n\r\n")[1]
-                        print(data)
-                    f.write(bytes(data,'utf-8'));
+                        data = bytes(data,'utf-8')
+                    print(data)
+                    f.write(data);
+        """
         # Close the connection when completed
-        
-def downloadUdpFile(socket,down_file):
-    count = 0;
-    headers=""
-    with open(down_file, 'wb') as f:
-        while True:
-            msg, (addr, port) = socket.recvfrom( 100 )
-            if resp.decode("ASCII") == "":
-                break
-            print(msg)
-            time.sleep(0.2)
-        f.close();
-               
+
 if __name__ == "__main__":
-    my_ip = "10.7.60.225";
-    tcpudp = "u"
-    ip="localhost"
-    port = "80"
-    down_file_dir = "/CNAssignment/filetodownload.txt";
-    down_file = down_file_dir.split('/')[-1]
-    if (tcpudp == "t"):    
-        # print('Number of arguments:', len(sys.argv), 'arguments.')
-        # print('Argument List:', str(sys.argv[0]))
+    #step 1: Read command line arguments
+    print(sys.argv)
+    resume=False;
+    connections=0
+    tInterval=0;
+    cType=""
+    filelocweb = "";
+    filewebname = "";
+    filelocpc = "";
+    filepcname = "";
+    for i in range(0,len(sys.argv)):
+        #Parse each of the flag and store the data corresponding to that.
+        if sys.argv[i]=="-r":
+            resume=True;
+        if sys.argv[i]=="-n":
+            connections=int(sys.argv[i+1])
+        if sys.argv[i]=="-i":
+            tInterval=float(sys.argv[i+1])
+        if sys.argv[i]=="-c":
+            cType = sys.argv[i+1]
+        if sys.argv[i]=="-f":
+            filelocweb = sys.argv[i+1];
+            filewebname = filelocweb.split('/')[-1]
+        if sys.argv[i]=="-o":
+            filelocpc = sys.argv[i+1]
+            filepcname = filelocpc.split('/')[-1]
+    print(resume,connections,tInterval,cType,filelocpc,filelocweb);
+    #Step 2: Using them to proper fixes
+    pcIp = "10.7.60.225"
+    serverHost=filelocweb.split("://")[1].split("/")[0];
+    serverPort = "80"
+    pcSockets = []
+    port=15000;
+    #First Extract Header informaton
+
+
+    #Making Multiple Connections
+    if (cType.lower() == "tcp"):    
+        print("TCP")
+        for i in range(connections):
+            #Create Socket TCP
+            s = makeTcpIpSocket();
+
+            makeTcpConnect(s,serverHost,serverPort)
+            pcSockets.append(s)
+            #Make TCP Connection
+            port+=1;
+        #Assign Each Connection to a Thread for downloading
+
+        #Generate a Request for file download
+        for s in pcSockets:
+            s.close();
         
-        #Create Socket TCP
-        s = makeTcpIpSocket();
-        #Make TCP Connection
-        makeTcpConnect(s,ip,port)
-        # Generate a Request for file download
-        makeTcpRequest(s,down_file_dir,"localhost")
-        downloadTcpFile(s,down_file)
-        
-        
+    """        
     elif (tcpudp == "u"):
         s = makeUdpIpSocket();
-        makeUdpRequest(my_ip,s,down_file_dir,"localhost")
+        makeUdpRequest(my_ip,s,down_file_dir,ip)
+        s.bind(("localhost",81))
         downloadUdpFile(s,down_file)
-                
+    """
     s.close()
     print ("\ndone")
     
